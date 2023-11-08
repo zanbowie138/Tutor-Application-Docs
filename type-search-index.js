4ec7d77f72c7550bb14c2be1e2cc00874cb57986
breadpear1080 <72674604+zanbowie138@users.noreply.github.com>
2023-11-08 13:49:28 -0600

    update

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Session"},{"p":"<Unnamed>","l":"Student"},{"p":"<Unnamed>","l":"Tutor"}];updateSearchResults();
